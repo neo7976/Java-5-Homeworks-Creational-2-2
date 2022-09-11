9f7b73c037a0b84a9ca28c56d7b7826ac5c86bcb
Dmitrii Sobin <neo7976@yandex.ru>
2022-09-11 14:55:21 +0300

    BinOps.java

--- src/main/java/BinOps.java
+++ src/main/java/BinOps.java
@@ -1,2 +1,19 @@
 public class BinOps {
+    public String sum(String a, String b) {
+        /**
+         * Переведите числа из двоичной записи в
+         строках в int, сложите и переведите
+         обратно в двоичную запись
+         */
+        return String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+    }
+
+    public String mult(String a, String b) {
+        /**Переведите числа из двоичной записи в
+         строках в int, перемножьте и переведите
+         обратно в двоичную запись
+         */
+        return String.valueOf(Integer.parseInt(a) * Integer.parseInt(b));
+    }
 }
+
